7a0d48e4f6635867eb5f08fd7941af278affcfd4
TiagoG <T.F.FigueiraGrelo@bradford.ac.uk>
2020-09-27 18:31:45 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,35 @@
+const Discord = require ('discord.js');
+const client = new Discord.Client();
+
+const prefix = '%';
+
+
+
+client.once('ready',() => {
+    console.log('Textbot is online!');
+});
+
+client.on('message', message =>{
+    if(!message.content.startsWith(prefix) || message.author.bot ) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if(command === 'ping'){
+        message.channel.send('pong');
+        
+    }
+    else if (command === 'pisoc'){
+        message.channel.send('JOIN PISOC!');
+    }
+    else if (message.content === `${prefix}server`) {
+        message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
+    }
+    
+
+});
+
+client.login();
+
+
+/*Made by Tiago */
